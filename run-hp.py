ed8982014025a9d283fa052caa4257d9848deacf
Miguel Lopes <miguelrtlopes@gmail.com>
2023-12-14 14:12:54 -0500

    bug with hp-runner

--- run-hp.py
+++ run-hp.py
@@ -2,7 +2,15 @@ import os
 import subprocess
 import sys
 import torch
+import logging
 from configurations import CONFIGURATIONS  # Importing the configuration file
+
+logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+                logging.FileHandler("run-hp.log"),
+                logging.StreamHandler()])
 
 def get_gpu_id(current_gpu_id, num_gpus):
     if num_gpus == 0:
@@ -15,12 +23,14 @@ def start_hp_tuner(num_instances, script_name='hp-tuner.py'):
 
     for instance in range(num_instances):
         current_gpu_id = get_gpu_id(current_gpu_id, num_gpus)
-        period_years = CONFIGURATIONS.get('period_years', 1)  # Default value if not set
+        period_years = CONFIGURATIONS.get('period_years', [1])[0]  # Default to [1] if not set
         args = [sys.executable, script_name, '--gpu_id', str(current_gpu_id),
                 '--num_instances', str(num_instances), '--period_years', str(period_years)]
         subprocess.Popen(args)
+        logging.info("Started instance")
 
 if __name__ == "__main__":
     # Reading number of instances from environment variable or defaulting to 2
     num_instances = int(os.getenv('NUM_INSTANCES', 2))
+    logging.info("Starting instance")
     start_hp_tuner(num_instances)
--- start_services.sh
+++ start_services.sh
@@ -4,11 +4,11 @@
 service mysql start
 
 # Start hp-tuner.py in the background
-python run-hp.py &
+python run-hp.py 
 
 sleep 5
 
 # Start optuna-dashboard
-nohup optuna-dashboard mysql+pymysql://optuna_user:r00t4dm1n@localhost/optuna_example --host "0.0.0.0"
+optuna-dashboard mysql+pymysql://optuna_user:r00t4dm1n@localhost/optuna_example --host "0.0.0.0"
 
 
