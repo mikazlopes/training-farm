e7047de2ec25ef613325221e51bf3da501e70652
Miguel Lopes <miguelrtlopes@gmail.com>
2023-12-14 21:49:15 -0500

    multimode

--- start_services.sh
+++ start_services.sh
@@ -9,15 +9,15 @@ num_instances=${NUM_INSTANCES:-2}
 # Start hp-tuner.py in the background with the specified number of instances
 python hp-tuner.py --period_years 10 --num_instances "$num_instances" &
 
+# Sleep for a short period to ensure hp-tuner.py starts properly
 sleep 5
 
-# Start optuna-dashboard
-optuna-dashboard mysql+pymysql://optuna_user:r00t4dm1n@localhost/optuna_example --host "0.0.0.0" &
+# Start optuna-dashboard if STUDY_MODE is not "client"
+if [ "$STUDY_MODE" != "client" ]; then
+    optuna-dashboard mysql+pymysql://optuna_user:r00t4dm1n@localhost/optuna_example --host "0.0.0.0" &
+fi
 
-# Wait for optuna-dashboard to be up
-sleep 5
-
-# Prompt the user after run-hp.py has finished
+# Wait for user input to stop the container
 while true; do
     read -p "Press y to stop the container: " input
     if [[ $input == "y" ]]; then
